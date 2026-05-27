c57ed58cabfc8ac900d1760d0792e1833f9d05c6
Mygod <contact-git@mygod.be>
2026-05-27 12:18:50 -0400

    v1.1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "be.mygod.datasimtile"
         minSdk = 24
         targetSdk = 36
-        versionCode = 3
-        versionName = "1.1.0"
+        versionCode = 4
+        versionName = "1.1.1"
     }
 
     buildFeatures {
